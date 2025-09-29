131c107665c1b67ecfaa2b2e0c7b90a57e0ff4ca
Sheena Ocon <oconsheen@gmail.com>
2025-09-29 17:04:33 +0800

    Modified config.php for deploying db to Railway

--- dB/config.php
+++ dB/config.php
@@ -3,15 +3,25 @@
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$servername = "localhost";
+// $servername = "localhost";
+// $username   = "root";
+// $password   = "";
+// $database   = "cleanaid";
+
+$servername = "mysql.railway.internal";
 $username   = "root";
-$password   = "";
-$database   = "cleanaid";
+$password   = "gMhexrzIPxuhRNOziuCWwTTJEsGXdvQy";
+$database   = "railway";
+$port      = 3306;
 
 try {
-    $conn = new mysqli($servername, $username, $password, $database);
+    // $conn = new mysqli($servername, $username, $password, $database);
+    $conn = new mysqli($servername, $username, $password, $database, $port);
     $conn->set_charset("utf8mb4");
 } catch (mysqli_sql_exception $e) {
     http_response_code(500);
     die("Database connection failed.");
 }
+
+//MYSQL_URL (from Railway):
+//mysql://root:gMhexrzIPxuhRNOziuCWwTTJEsGXdvQy@mysql.railway.internal:3306/railway
